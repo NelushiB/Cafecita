16bafd0b129d79196e4f3d021c1e24f85a64decb
Nelushi Bamunusinghe <nelushib@hotmail.com>
2024-07-23 17:03:24 +0200

    Implementing responsive design for pages

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
    const header = document.querySelector('.header');
+   const burger = document.querySelector('.burger');
+   const menu = document.querySelector('.menu');
    const sticky = header.offsetTop;
    const faqs = document.querySelectorAll('.faq');
 
+   //Burger Button
+   burger.addEventListener('click', () => { menu.classList.toggle('wide')});
+
+   // Sticky Navbar
    window.onscroll = function() {
       if(window.pageYOffset > sticky) {
          header.classList.add('sticky');
@@ -11,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
    }
 
+   // Scroll Single Faq
    faqs.forEach((faq) => {
       faq.addEventListener('click', () => {
          faq.classList.toggle('active-faq');
